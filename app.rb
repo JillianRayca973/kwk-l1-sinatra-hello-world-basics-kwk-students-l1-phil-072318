96fdd5d180659c514fbc7a932d6b3c7b6660d0bb
Jill  Rayca <rkrayca@yahoo.com>
2018-07-31 16:53:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+ get '/' do
+   "Hello World!Hello darkness my old friend. Ive come to talk with you again. because a vision softly creeping left its seeds while i was sleeping. and the vision that was planted in my brain still remains within the sound of silence"
+ end
+ 
 end
